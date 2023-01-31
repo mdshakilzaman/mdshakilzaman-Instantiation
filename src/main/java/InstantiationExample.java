6e0d2a34fed688d3ea2136f57512af440527a3a4
Md Shakil Zaman <mdshakilzaman2020@gmail.com>
2023-01-31 02:49:53 +0000

    first commit

--- src/main/java/InstantiationExample.java
+++ src/main/java/InstantiationExample.java
@@ -17,7 +17,10 @@ public class InstantiationExample {
      *
      * @return any object.
      */
+    public static void main(String[] args){
+    }
     public Object instantiateANewObject(){
-        return null;
+        String name = new String();
+        return name;
     }
 }
